95f52c36a20f2c68f028a8d97bc52705c8f72e54
Matthew Tubbs <matthtubbs@gmail.com>
2025-04-30 08:47:43 -0600

    wrote code to read the data and language files

--- read-csv-app.js
+++ read-csv-app.js
@@ -2,10 +2,23 @@
 import csv from 'csv-parser';
 import fs from 'fs';
 
+const language = process.argv[2];
+
 fs.createReadStream('data/data.csv')
     .pipe(csv())
     .on('data', (row) => {
-        console.log(row);
+        console.log(row)
+    })
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+    })
+
+fs.createReadStream('data/languages.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        if (row.language === language) {
+            console.log(row);
+        }
     })
     .on('end', () => {
         console.log('CSV file successfully processed');
